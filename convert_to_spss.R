05f6d35c4f3676cafdcaa0f2c6525217fb01beb4
Alex Kim <dev@ajpkim.com>
2024-04-27 10:42:09 -0600

    Update script to include value labels

--- convert_to_spss.R
+++ convert_to_spss.R
@@ -30,15 +30,14 @@ load_files <- function(encoded_data_path, data_dict_path) {
     tmp_data <- read_csv(encoded_data_path, col_names = FALSE, show_col_types = FALSE)
     col_names <- as.character(tmp_data[1, ])
     # Reload the data, skipping the first two rows to avoid the col description row
-    encoded_data <- read_csv(encoded_data_path, skip = 2, col_names = col_names, show_col_types = FALSE)
+    data <- read_csv(encoded_data_path, skip = 2, col_names = col_names, show_col_types = FALSE)
 
-    return(list(encoded_data = encoded_data, data_dict = data_dict))
+    return(list(data = data, data_dict = data_dict))
 }
 
-
+# Important for adding variable labels for SPSS.
+# The 2nd row in the encoded data files contain column descriptions.
 get_col_descriptions <- function(encoded_data_path) {
-    # The 2nd row in the encoded data files are columnd description
-    # which we pass along to SPSS as variable labels.
     df <- read_csv(encoded_data_path, col_names = FALSE, show_col_types = FALSE)
     col_names <- as.character(df[1, ])
     col_descriptions <- as.character(df[2, ])
@@ -47,38 +46,7 @@ get_col_descriptions <- function(encoded_data_path) {
     return(col_descriptions)
 }
 
-decode_data <- function(encoded_data, data_dict) {
-    # Ensure data_dict has proper columns
-    if (!("variable" %in% names(data_dict)) || !("label" %in% names(data_dict)) || !("value" %in% names(data_dict))) {
-        stop("data_dict must contain 'variable', 'label', and 'value' columns.")
-    }
-
-    # Loop over the coded variables defined in data_dict and replace encoded values
-    for (var in unique(data_dict$variable)) {
-        if (var %in% names(encoded_data)) {
-            # Extract mapping for this variable
-            mapping <- data_dict %>%
-                       filter(variable == var)
-
-            # Replace encoded numeric codes with descriptive text values
-            # Find matches for each value in the column
-            matches <- match(encoded_data[[var]], mapping$label)
-
-            # Use matches to replace only those values that have a corresponding match in the data dictionary
-            # NA values in 'matches' mean there was no corresponding key in 'mapping$label' for that value
-            decoded_values <- ifelse(is.na(matches), encoded_data[[var]], mapping$value[matches])
-
-            # Convert the entire column to character type, now safely handling non-matching cases
-            encoded_data[[var]] <- as.character(decoded_values)
-        } else {
-            warning(paste("Variable", var, "not found in encoded_data."))
-        }
-    }
-    return (encoded_data)
-}
-
-
-
+# Add variable labels as metadata for SPSS
 add_var_labels <- function(data, encoded_data_path) {
     # Add variable labels for SPSS from the 2nd row of encoded data files
     col_descriptions <- get_col_descriptions(encoded_data_path)
@@ -91,43 +59,73 @@ add_var_labels <- function(data, encoded_data_path) {
     return (data)
 }
 
-get_categorical_cols <- function(decoded_data) {
-    # Identify numeric columns first
-    numeric_cols <- names(decoded_data)[sapply(decoded_data, is.numeric)]
-    # Define the pattern to match column names starting with 'Q' followed by numbers and possibly a decimal
-    pattern <- "^Q\\d+(\\.\\d+)?"
-    # Filter numeric column names based on the pattern
-    cat_cols <- grep(pattern, numeric_cols, value = TRUE)
+convert_categorical_cols_to_factors <- function(data) {
+    # Define columns to exclude from being converted to factors
+    exclude_cols <- c("ResponseId", "RecordedDate", "institution_institutionName",
+                      "institution_totalEnrollment", "institution_undergraduateEnrollment")
 
-    return(cat_cols)
-}
+    # Get all categorical col names that are not in the exclude list
+    cat_cols <- names(data)[sapply(data, is.numeric) & !names(data) %in% exclude_cols]
 
-convert_categorical_cols_to_factors <- function(decoded_data) {
-    # Identify all the categorical cols we want to convert to factors
-    cat_cols <- get_categorical_cols(decoded_data)
-    # Loop through the cat_cols and convert each specified column to a factor
+    # Loop through the cat_cols and convert each to a factor
     for (col in cat_cols) {
-        decoded_data[[col]] <- as.factor(decoded_data[[col]])
+        data[[col]] <- factor(data[[col]])
     }
-    return(decoded_data)
+
+    return(data)
 }
+
+# Parse the data dict for the encoded variable values
+# and add as value labels for SPSS file.
+add_factor_labels <- function(data, data_dict) {
+    unique_vars <- unique(data_dict$variable)
+
+    for (var in unique_vars) {
+        if (var %in% names(data)) {
+            var_dict <- filter(data_dict, variable == var)
+
+            # Ensure levels and labels are character vectors
+            levels <- as.character(var_dict$label)
+            labels <- as.character(var_dict$value)
+
+            # Truncate labels if they exceed 120 characters bc of SPSS rules
+            truncated_labels <- sapply(labels, function(label) {
+                if (nchar(label) > 120) {
+                    new_label <- paste(substr(label, 1, 116), "...")
+                    return(new_label)
+                } else {
+                    return(label)
+                }
+            })
+
+            # Convert column to factor with appropriate levels and labels
+            # Ensure that labels are correctly aligned with levels
+            data[[var]] <- factor(data[[var]], levels = levels, labels = truncated_labels)
+        }
+    }
+    return(data)
+}
+
+
 
 main <- function(encoded_dath_path, data_dict_path, spss_output_path) {
     # Import the encoded data and the data dict
     result <- load_files(encoded_data_path, data_dict_path)
-    encoded_data <- result$encoded_data
+    data <- result$data
     data_dict <- result$data_dict
-    # Decode data
-    data <- decode_data(encoded_data, data_dict)
-    # Add variable labels for SPSS
-    data <- add_var_labels(data, encoded_data_path)
+
     # Convert categorical cols to factor type
     data <- convert_categorical_cols_to_factors(data)
+
+    # Add variable labels for SPSS
+    data <- add_var_labels(data, encoded_data_path)
+
+    # Add factor/value labels for SPSS
+    data <- add_factor_labels(data, data_dict)
+
     # Export the processed data to an SPSS file
     write_sav(data, spss_output_path)
     print(paste("Data exported successfully to", spss_output_path))
-
-    # return(data)
 }
 
 main(encoded_data_path, data_dict_path, spss_output_path)
